e0e7971f8aec2320d29f061efba98b2cddaeb340
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-11-18 14:19:15 -0300

    chore: add new root colors

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,7 +2,10 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   :root{
-    --body-color: #15171a
+    --body-color: #15171a;
+    --button-form-color: #3366ff;
+    --text-primary-color: #ffffff;
+    --text-secondary-color: #b4b8bb;
   }
 
   * {
